7102573293a4cd33ee488ecb984314ffcb588423
bernabarros <bernardobarros.work@gmail.com>
2025-04-04 17:41:45 +0100

    Add Hero class code

---
+++ MyRoguelike/Hero.cs
@@ -0,0 +1,75 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyRoguelike
+{
+    public class Hero
+    {
+        private int xp;
+        private float health;
+        public string Name{get;}
+        public int XP
+        {
+            get
+            {
+                return xp;
+            }
+            set
+            {
+                if(value > 0)
+                {
+                    xp += value;
+                }
+            }
+        }
+        public int Level
+        {
+            get
+            {
+                return 1 + xp / 1000;
+            }
+        }
+        public float Health
+        {
+            get
+            {
+                return health;
+            }
+            set
+            {
+                health += value;
+
+                if(health < 0)
+                {
+                    health = 0;
+                }
+                else if(health > MaxHealth)
+                {
+                    health = MaxHealth;
+                }
+            }
+        }
+        public float MaxHealth
+        {
+            get
+            {
+                return (float)100 + (Level-1)*20;
+            }
+        }
+    public Hero(string name)
+    {
+        Name = name;
+        xp = 0;
+        Health = MaxHealth;
+    }
+
+        public void TakeDamage(float damage)
+        {
+            health -= damage;
+            xp += (int)damage /20;
+        }
+
+    }
+}
